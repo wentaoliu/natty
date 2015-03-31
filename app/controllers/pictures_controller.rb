b1c060f4e0ec8d8048c50ba686524b753efdd02e
Wentao Liu <wentaoliu@live.com>
2015-03-31 23:34:58 +0800

    fix the returned path of uploaded images

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -5,7 +5,9 @@ class PicturesController < ApplicationController
     @picture = Picture.new(image: params[:upload_file])
     @picture.save
     # Return the file path
-    render json:{ file_path: @picture.image.url(:medium) }
+    render json:{
+      file_path: (ENV["RAILS_RELATIVE_URL_ROOT"]||'') + @picture.image.url(:medium)
+    }
   end
 
 end
